df0471d1d7a6cb32c13c98482ff77a50d898c4a8
diana-mkrtchyan <62055439+diana-mkrtchyan@users.noreply.github.com>
2020-03-11 14:18:59 +0400

    Create Ingredient_checking.py

---
+++ Ingredient_checking.py
@@ -0,0 +1,64 @@
+biscuit_recpie = "Eggs, sugar, yogurt, oil, soda, vanilla, flour"
+biscuit_time = 50 
+
+user_time = int(input("How many minutes do you have?"))
+All_ingrediants_available = True
+
+if (user_time < biscuit_time):
+  print("Sorry you don't have time")
+else:
+  #All ingredients should be mentioned here
+  eggs_available = input("Do you have eggs? Please answer yes or no")
+  sugar_available = input("Do you have sugar? Please answer yes or no")
+  yogurt_available = input("Do you have yogurt? Please answer yes or no")
+  oil_available = input("Do you have oil? Please answer yes or no")
+  soda_available = input("Do you have soda? Please answer yes or no")
+  vanilla_available = input("Do you have vanilla? Please answer yes or no")
+  flour_available = input("Do you have flour? Please answer yes or no")
+
+  if(eggs_available == "yes"):
+    print("Check the next ingrediant")
+  else:
+    print("You need to find it")
+    All_ingrediants_available = False
+
+  if(sugar_available == "yes"):
+    print("Check the next ingrediant")
+  else:
+    print("You need to find it")
+    All_ingrediants_available = False
+
+  if(yogurt_available == "yes"):
+    print("Check the next ingrediant")
+  else:
+    print("You need to find it")
+    All_ingrediants_available = False
+
+  if(oil_available == "yes"):
+    print("Check the next ingrediant")
+  else:
+    print("You need to find it")
+    All_ingrediants_available = False
+
+  if(soda_available == "yes"):
+    print("Check the next ingrediant")
+  else:
+    print("You need to find it")
+    All_ingrediants_available = False
+
+  if(vanilla_available == "yes"):
+    print("Check the next ingrediant")
+  else:
+    print("You need to find it")
+    All_ingrediants_available = False
+
+  if(flour_available == "yes"):
+    print("Check the next ingrediant")
+  else:
+    print("You need to find it")
+    All_ingrediants_available = False
+
+  if(All_ingrediants_available == True):
+    print("Good luck with your cooking")
+  else:
+    print("Oh Oh! Check another option")
