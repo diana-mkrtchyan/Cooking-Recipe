9ef9e138fd3af9b01f4167198f8014b735a6d16e
Kristine2020 <62056033+Kristine2020@users.noreply.github.com>
2020-03-23 18:57:08 +0400

    Update Alternative.py

--- Alternative.py
+++ Alternative.py
@@ -1,31 +1,73 @@
 oreoCake_recpie = {
-  "time" : "90",
-  "ingrediants for biscuit" : {
+  "time" : {
+    "portion" : 90,
+    "metric" : "minutes",
+  },
+  "ingredients for biscuit" : {
     "egg" : 6,
     "sugar" : {
-      "sugar" : "300 gram",
-      "alternative" : ["powdered sugar 400 gram" ]
+      "portion" : 300,
+      "metric" : "grams",
+      "alternative" : {
+        "name" : "powedered sugar",
+        "portion" : 400,
+        "metric" : "grams"
+      }
     },
-    "cacao" : "50 gram",
-    "oil" : "100 gram",
-    "vanila" : "1 pinch",
+    "cacao" : {
+      "portion" : 50,
+      "metric" : "grams"
+    },
+    "oil" : {
+      "portion" : 100,
+      "metric" : "grams"
+    },
+    "vanila" : {
+      "portion" : 1,
+      "metric" : "pinch"
+    },
     "baking powder" : {
-      "baking powder" : "10 gram",
-      "alternative" : "soda 10 gram"
-    },
-    "flour" : "150 gram"
-   },
-   "ingrediants for cream" : {
-     "cream cheese" : {
-       "cream cheese" : "500-600 gram",
-       "alternative" : "cottage cheese 500-600 gram"
+      "portion" : 10,
+      "metric" : "grams",
+      "alternative" : {
+        "name" : "soda",
+        "portion" : 10,
+        "metric" : "grams"
       },
-     "heavy cream" : "500 ml",
-     "powdered sugar" : "150 gram",
-     "oreo cookies" : "20 pieces",
-     "dark chocolate" : "50 gram"
-   },
+    },
+    "flour" : {
+      "portion" : 150,
+      "metric" : "grams"
+    },
+  },
+  "ingredients for cream" : {
+    "cream cheese" : {
+      "portion" : 500 or 600,
+      "metric" : "grams",
+      "alternative" : {
+         "name" : "cottage cheese",
+         "portion" : 500 or 600,
+         "metric" : "grams"
+      }
+    },
+    "heavy cream" : {
+      "portion" : 500,
+      "metric" : "milliliters"
+    },
+    "powdered sugar" : {
+      "portion" : 150,
+      "metric" : "grams"
+    },
+    "oreo cookies" : {
+      "portion" : 20,
+      "metric" : "pieces"
+    },
+    "dark chocolate" : {
+      "portion" : 50,
+      "metric" : "grams"
+    },
+  },
 }
 
-dataToPrint = oreoCake_recpie["ingrediants for cream"]["cream cheese"]["alternative"]
+dataToPrint = oreoCake_recpie ["ingredients for cream"] ["cream cheese"] ["alternative"] ["name"]
 print(dataToPrint)
