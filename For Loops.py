aaf0e03488b074d5f0e72ec582b3fcd92242845c
diana-mkrtchyan <62055439+diana-mkrtchyan@users.noreply.github.com>
2020-03-25 00:45:33 +0400

    Create For Loops.py

---
+++ For Loops.py
@@ -0,0 +1,19 @@
+cake_type = [
+  "oreoCake",
+  "cheeseCake",
+  "snickersCake",
+  "caramelCake",
+]
+
+for name in cake_type:
+  print(name)
+
+pricing_list = [ 
+"oreoCake_15000AMD",
+"cheeseCake_13000AMD",
+"snickersCake_16000AMD",
+"caramelCake_10000AMD",
+]
+
+for price in pricing_list:
+  print(price)
