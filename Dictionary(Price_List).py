d45661b104e09af543ee89ed142d8954557f8ac4
diana-mkrtchyan <62055439+diana-mkrtchyan@users.noreply.github.com>
2020-03-25 19:49:20 +0400

    Update and rename For Loops.py to Dictionary(Price_List).py

---
+++ Dictionary(Price_List).py
@@ -0,0 +1,37 @@
+price_list = {
+  "oreoCake" : {
+    "price" : 12000,
+    "currency" : "AMD"
+  }, 
+  "strawberryCake" : {
+    "price" : 11000,
+    "currency" : "AMD"
+  },
+  "snickersCake" : {
+    "price" : 13000,
+    "currency" : "AMD"
+  },
+  "caramelCake" : {
+    "price" : 10000,
+    "currency" : "AMD"
+  },
+  "cheeseCake" : {
+    "price" : 14000,
+    "currency" : "AMD"
+  },
+  "chocolateCake": {
+    "price" : 9000,
+    "currency" : "AMD"
+  },
+  "lakomkaCake" : {
+    "price" : 8000,
+    "currency" : "AMD"
+  },
+  "limonCake" : {
+    "price" : 7000,
+    "currency" : "AMD"
+  },
+}
+
+dataToPrint = price_list ["oreoCake"]["price"]
+print(dataToPrint)
